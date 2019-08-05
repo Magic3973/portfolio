ee05d3cedc5976fac0289a20dbbd10497c198ada
magic3973 <isa-tey@orange.fr>
2019-08-05 07:35:44 +0000

    5 aout au matin

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/js/app.js')
-    .addStyleEntry('script', './assets/js/center.js')
+    .addEntry('script', './assets/js/center.js')
 
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
